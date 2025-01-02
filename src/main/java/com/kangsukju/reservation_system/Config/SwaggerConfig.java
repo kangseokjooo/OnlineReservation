2fcbf6c384dcd1a33a4a186958102d4004bb7afe
kangseokjooo <zpckq69@naver.com>
2025-01-02 12:12:24 +0900

    First Commit

---
+++ src/main/java/com/kangsukju/reservation_system/Config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.kangsukju.reservation_system.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Online Reservation Swagger")
+                .description("온라인 예약 시스템 RestFul API")
+                .version("1.0.0");
+    }
+}
---
+++ src/test/java/com/kangsukju/reservation_system/OnlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangsukju.reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
